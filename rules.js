40e836a541e6ecf2ba0471489b9937cfdfbeab8b
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2016-02-15 03:18:09 -0800

    put a ruleset name in the rule json

--- rules.js
+++ rules.js
@@ -1,4 +1,6 @@
-var original_definitions = [
+var original_definitions = {
+ruleset_name: "Durdle Dwarves v1",
+rules: [
 {
     name: "Spawn on platform",
 
@@ -343,21 +345,21 @@ var original_definitions = [
             "**O**"+
             "*****"
 }
-
 ]
+}
 
 var rule_definitions = original_definitions;
 
 //turn the rules into a list of which squares we care about and a list of which squares get changed
 function preprocessRules(rule_definitions) {
     var new_rules = [];
-    for (var i = 0; i < rule_definitions.length; ++i) {
-        var defn = rule_definitions[i];
+    for (var i = 0; i < rule_definitions.rules.length; ++i) {
+        var defn = rule_definitions.rules[i];
         var new_pattern = [];
         var new_outcome = [];
         defn.pattern = defn.pattern.replace(/(\r\n|\n|\r)/gm,"");
         if (defn.pattern.length != 25) {
-            setRuleErrorMsg("Rule '" + defn.name + "' must have 25 symbols in its pattern")
+            setRuleErrorMsg("Rule '" + defn.name + "' must have exactly 25 symbols in its pattern")
             return null;
         }
         if (defn.pattern[12] != "O") {
@@ -373,7 +375,7 @@ function preprocessRules(rule_definitions) {
 
         defn.outcome = defn.outcome.replace(/(\r\n|\n|\r)/gm,"");
         if (defn.outcome.length != 25) {
-            setRuleErrorMsg("Rule '" + defn.name + "' must have 25 symbols in its outcome")
+            setRuleErrorMsg("Rule '" + defn.name + "' must have exactly 25 symbols in its outcome")
             return null;
         }
 
@@ -408,9 +410,8 @@ function populatePageWithRules(rule_definitions) {
     var rule_container = document.getElementById("rules-container");
     rule_container.innerHTML = ""
 
-
-    for(var i = 0; i < rule_definitions.length; ++i) {
-        var rule_defn = rule_definitions[i];
+    for(var i = 0; i < rule_definitions.rules.length; ++i) {
+        var rule_defn = rule_definitions.rules[i];
 
         var rule_div = document.createElement("div");
         rule_div.className = "rule-square";
@@ -447,6 +448,8 @@ function populatePageWithRules(rule_definitions) {
 
         rule_container.appendChild(rule_div);
     }
+
+    $("#ruleset-name")[0].value = rule_definitions.ruleset_name;
 }
 
 
@@ -456,7 +459,8 @@ function applyRuleChanges() {
     }
     var rule_container = document.getElementById("rules-container");
     var rule_divs = rule_container.getElementsByTagName("div");
-    var new_definitions = [];
+    var ruleset_name = document.getElementById("ruleset-name").value;
+    var new_definitions = {name: ruleset_name, rules: []};
     for (var i = 0; i < rule_divs.length; ++i) {
         var rule_div = rule_divs[i];
         var new_definition = {};
@@ -464,7 +468,7 @@ function applyRuleChanges() {
         new_definition.pattern = rule_div.getElementsByClassName("pattern-textarea")[0].value;
 
         new_definition.outcome = rule_div.getElementsByClassName("outcome-textarea")[0].value;
-        new_definitions.push(new_definition);
+        new_definitions.rules.push(new_definition);
     }
 
     var new_rules = preprocessRules(new_definitions);
@@ -473,6 +477,10 @@ function applyRuleChanges() {
         rule_definitions = new_definitions;
         hist.latest_idx = hist.curr_idx;
         setRuleErrorMsg("");
+        var ruleset_name_display = $("#ruleset-name")[0];
+        if (ruleset_name_display.value === original_definitions.ruleset_name) {
+            ruleset_name_display.value = "Custom rules";
+        }
     }
 }
 
